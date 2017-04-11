17f9bc1b020e111d04e420093e42b485fb72bf9d
SarathBabuG <sarath.gannamani@gmail.com>
2017-04-11 09:21:44 +0530

    Create mysql-connector.py

---
+++ mysql-connector.py
@@ -0,0 +1,45 @@
+import sys, json, base64
+import MySQLdb
+
+db_host   = "localhost"
+db_name   = "msp"
+db_user   = "msp"
+db_passwd = "bXNw"
+
+dbconn = None
+
+def cursorFilter(func):
+
+    def wrapper(*args, **kwargs):
+        global dbconn
+        if not dbconn:
+            ''' Create the database connection only once '''
+            dbconn = MySQLdb.connect(host = db_host, user = db_user, passwd = base64.decodestring(db_passwd), db = db_name)
+
+        try:
+            cursor = dbconn.cursor()
+            try:
+                result = func(cursor, *args, **kwargs)
+            finally:
+                cursor.close()
+            dbconn.commit()
+        except:
+            dbconn.rollback()
+            raise
+        return result
+    return wrapper
+
+
+@cursorFilter
+def execute_query(cursor, query, *args):
+    try:
+        cursor.execute(query, (args))
+        if query.lower().startswith("select"):
+            return cursor.fetchall()
+        return True
+    except Exception, emsg:
+        print "execute_query error: %s" % (emsg)
+        return False
+
+
+execute_query("select * from sample;")
